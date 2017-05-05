bca69d8ab317bf5a344181d1caa7c1fbee4a8c7a
Klimaszewski,Szymon <klimaszewskis@ryanair.com>
2017-05-05 14:01:14 +0200

    Add backend example

---
+++ Services/backend/src/main/java/mobi/klimaszewski/linguist/services/backend/MyBean.java
@@ -0,0 +1,17 @@
+package mobi.klimaszewski.linguist.services.backend;
+
+/**
+ * The object model for the data we are sending through endpoints
+ */
+public class MyBean {
+
+    private String myData;
+
+    public String getData() {
+        return myData;
+    }
+
+    public void setData(String data) {
+        myData = data;
+    }
+}
---
+++ Services/backend/src/main/java/mobi/klimaszewski/linguist/services/backend/MyEndpoint.java
@@ -0,0 +1,50 @@
+/*
+   For step-by-step instructions on connecting your Android application to this backend module,
+   see "App Engine Java Endpoints Module" template documentation at
+   https://github.com/GoogleCloudPlatform/gradle-appengine-templates/tree/master/HelloEndpoints
+*/
+
+package mobi.klimaszewski.linguist.services.backend;
+
+import com.google.api.server.spi.config.Api;
+import com.google.api.server.spi.config.ApiMethod;
+import com.google.api.server.spi.config.ApiNamespace;
+import com.google.appengine.api.datastore.Text;
+import com.google.cloud.datastore.Datastore;
+import com.google.cloud.datastore.DatastoreOptions;
+import com.google.cloud.datastore.Entity;
+import com.google.cloud.datastore.Key;
+import com.google.cloud.datastore.Value;
+
+import javax.inject.Named;
+
+/**
+ * An endpoint class we are exposing
+ */
+@Api(
+        name = "myApi",
+        version = "v1",
+        namespace = @ApiNamespace(
+                ownerDomain = "backend.services.linguist.klimaszewski.mobi",
+                ownerName = "backend.services.linguist.klimaszewski.mobi",
+                packagePath = ""
+        )
+)
+public class MyEndpoint {
+
+    /**
+     * A simple endpoint method that takes a name and says Hi back
+     */
+    @ApiMethod(name = "sayHi")
+    public MyBean sayHi(@Named("name") String name) {
+        MyBean response = new MyBean();
+        response.setData("Hi, " + name);
+        Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
+        Key key = datastore.newKeyFactory().setKind("Translation").newKey(name);
+        Entity entity = datastore.get(key);
+        Value<Text> original = entity.getValue("original");
+        response.setData(original.get().getValue());
+        return response;
+    }
+
+}
