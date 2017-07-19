0c4382b27efdfa5b0f9ce641c37148a9b63449f9
Klimaszewski,Szymon <klimaszewskis@ryanair.com>
2017-07-19 11:46:16 +0200

    Renaming

--- src/main/java/mobi/klimaszewski/linguist/RemoteAppService.java
+++ src/main/java/mobi/klimaszewski/linguist/RemoteAppService.java
@@ -11,7 +11,7 @@ import java.util.Map;
 import mobi.klimaszewski.services.TranslationConfig;
 import mobi.klimaszewski.services.TranslationInterface;
 
-public class TranslationService extends Service {
+public class RemoteAppService extends Service {
 
     private final IBinder binder = new TranslationInterface.Stub() {
 
