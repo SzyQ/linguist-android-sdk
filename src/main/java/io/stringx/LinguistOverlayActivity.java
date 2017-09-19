4868c94092b39d14a86ed19da8e05c5adf6a721e
Klimaszewski,Szymon <klimaszewskis@ryanair.com>
2017-09-19 13:48:19 +0200

    Handle package installation

--- src/main/java/io/stringx/LinguistOverlayActivity.java
+++ src/main/java/io/stringx/LinguistOverlayActivity.java
@@ -2,10 +2,17 @@ package io.stringx;
 
 
 import android.content.ActivityNotFoundException;
+import android.content.BroadcastReceiver;
 import android.content.ComponentName;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
@@ -15,7 +22,12 @@ import io.stringx.app.R;
 public final class LinguistOverlayActivity extends AppCompatActivity {
 
     public static final int REQUEST_CODE = 3001;
+    public static final String PACKAGE_NAME = "io.stringx";
     private static final int REQUEST_CODE_TRANSLATE = 3002;
+    public static final String STRINGX_ACTIVITY = "io.stringx.MainActivity";
+    public static final String KEY_PACKAGE = "KEY_PACKAGE";
+    private BroadcastReceiver receiver;
+    private boolean isWaitingForAppInstall;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -77,15 +89,87 @@ public final class LinguistOverlayActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent();
-                String packageName = "io.stringx";
-                intent.setComponent(new ComponentName(packageName, "io.stringx.MainActivity"));
-                intent.putExtra("KEY_PACKAGE",getPackageName());
+                intent.setComponent(new ComponentName(PACKAGE_NAME, STRINGX_ACTIVITY));
+                intent.putExtra(KEY_PACKAGE, getPackageName());
                 try {
                     startActivityForResult(intent, REQUEST_CODE_TRANSLATE);
                 } catch (ActivityNotFoundException e) {
-                    Utils.openStore(getApplicationContext(), packageName);
+                    CharSequence label;
+                    try {
+                        label = getPackageManager().getPackageInfo(getPackageName(), 0).applicationInfo.loadLabel(getPackageManager());
+                    } catch (PackageManager.NameNotFoundException e1) {
+                        label = getString(R.string.app);
+                    }
+                    new AlertDialog.Builder(LinguistOverlayActivity.this)
+                            .setTitle(R.string.dialog_title)
+                            .setMessage(label + " " + getString(R.string.dialog_message))
+                            .setPositiveButton(R.string.dialog_button, new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    registerPackageChangedReceiver();
+                                    Utils.openStore(getApplicationContext(), PACKAGE_NAME);
+                                }
+                            }).show();
                 }
             }
         });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    private void registerPackageChangedReceiver() {
+        if (receiver != null) {
+            return;
+        }
+        LL.d("Registering package listener");
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(Intent.ACTION_PACKAGE_ADDED);
+        intentFilter.addDataScheme("package");
+        receiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                LL.d("Package added: "+intent.getData());
+                Uri data = intent.getData();
+                if (data != null && PACKAGE_NAME.equals(data.getSchemeSpecificPart())) {
+                    Intent stringxIntent = new Intent();
+                    stringxIntent.setComponent(new ComponentName(PACKAGE_NAME, STRINGX_ACTIVITY));
+                    stringxIntent.putExtra(KEY_PACKAGE, getPackageName());
+                    try {
+                        startActivityForResult(stringxIntent, REQUEST_CODE_TRANSLATE);
+                    }catch (Exception ignored){
+                        LL.e("Failed to start stringx!",ignored);
+                    }
+//                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+//                        ActivityManager service = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+//                        for (ActivityManager.AppTask appTask : service.getAppTasks()) {
+//                            appTask.moveToFront();
+//                            break;
+//                        }
+                        unregisterReceiver();
+//                    }
+                }
+            }
+        };
+        registerReceiver(receiver, intentFilter);
+        isWaitingForAppInstall = true;
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver();
+    }
+
+    private void unregisterReceiver() {
+        if (receiver != null) {
+            try {
+                unregisterReceiver(receiver);
+            } catch (Exception ignored) {
+
+            }
+        }
     }
 }
