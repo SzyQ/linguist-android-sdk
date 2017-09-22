97f7cfbe71d5c8cde1acdab450334f872abaa4e6
Szymon Klimaszewski <klimaszewski.szymon@gmail.com>
2017-09-22 09:24:31 +0200

    Add possibility to exclude particular strings

--- samples/DeveloperSample/app/src/main/java/io/stringx/developersample/App.java
+++ samples/DeveloperSample/app/src/main/java/io/stringx/developersample/App.java
@@ -3,13 +3,13 @@ package io.stringx.developersample;
 import android.app.Application;
 
 import io.stringx.Language;
-import io.stringx.Linguist;
 import io.stringx.Options;
+import io.stringx.Stringx;
 import io.stringx.Translatable;
 
 public class App extends Application implements Translatable {
 
-    private Linguist linguist;
+    private Stringx linguist;
 
     @Override
     public void onCreate() {
@@ -17,14 +17,15 @@ public class App extends Application implements Translatable {
         Options options = new Options.Builder(this, Language.English)
                 .setMode(Options.Mode.Developer)
                 .addStrings(R.string.class)
+                .excludeString(R.string.app_name)
                 .excludeStrings(android.support.v4.R.string.class)
                 .excludeStrings(android.support.v7.appcompat.R.string.class)
                 .build();
-        linguist = new Linguist(this, options);
+        linguist = new Stringx(this, options);
     }
 
     @Override
-    public Linguist getLinguist() {
+    public Stringx getStringx() {
         return linguist;
     }
 }
