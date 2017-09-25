92d6a41b99bae44183da9d8b896709ee677ae274
Szymon Klimaszewski <klimaszewski.szymon@gmail.com>
2017-09-25 07:56:15 +0200

    Remove logging on debug

--- stringx/src/main/java/io/stringx/LL.java
+++ stringx/src/main/java/io/stringx/LL.java
@@ -2,26 +2,38 @@ package io.stringx;
 
 import android.util.Log;
 
+import io.stringx.client.BuildConfig;
+
 public class LL {
     public static final String TAG = "StringX";
 
     public static void d(String string) {
-        Log.d(TAG, string);
+        if (BuildConfig.DEBUG) {
+            Log.d(TAG, string);
+        }
     }
 
     public static void v(String string) {
-        Log.v(TAG, string);
+        if (BuildConfig.DEBUG) {
+            Log.v(TAG, string);
+        }
     }
 
     public static void e(String error, Throwable e) {
-        Log.e(TAG, error, e);
+        if (BuildConfig.DEBUG) {
+            Log.e(TAG, error, e);
+        }
     }
 
     public static void d(String s, Exception exception) {
-        Log.d(TAG, s, exception);
+        if (BuildConfig.DEBUG) {
+            Log.d(TAG, s, exception);
+        }
     }
 
     public static void w(String s) {
-        Log.w(TAG,s);
+        if (BuildConfig.DEBUG) {
+            Log.w(TAG, s);
+        }
     }
 }
