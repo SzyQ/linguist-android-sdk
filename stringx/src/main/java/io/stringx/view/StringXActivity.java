5a5d72e318419e9cdcb1174b50b6de3e4194d987
Szymon Klimaszewski <klimaszewski.szymon@gmail.com>
2017-10-19 21:17:23 +0200

    Cleanup imports

--- stringx/src/main/java/io/stringx/view/StringXActivity.java
+++ stringx/src/main/java/io/stringx/view/StringXActivity.java
@@ -1,10 +1,6 @@
 package io.stringx.view;
 
-import android.content.Context;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.app.AppCompatDelegate;
-import android.view.MenuInflater;
 
 import io.stringx.StringX;
 
